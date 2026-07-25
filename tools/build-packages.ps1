84c846ac2a6bd432e21c46536d0ff9fa068cd96e
rafalrozacki-debug <rafal.rozacki@gmail.com>
2026-07-25 21:36:56 +0200

    Fix: ujednolicenie numeru wersji do 1.7.0 w calym repo
    Wersja byla rozjechana w piec roznych miejsc: package.json mial
    "version": "1.1.0" (pole) i "1.6.2" w opisie, strona glowna i
    download.html pokazywaly 1.6.2, a stopka i najnowszy wpis w changelog
    juz mowily 1.7.0 - ale paczka do pobrania w dist/ wciaz byla w wersji
    1.6.2, wiec download.html linkowal do czegos niezgodnego z reszta
    strony.
    
    Ujednolicone do 1.7.0 (najnowszy, kompletnie opisany wpis w
    changelogu): package.json, badge na stronie glownej (PL/EN/DE), opis i
    linki na download.html (PL/EN/DE, wraz z realnymi rozmiarami nowej
    paczki), meta description w changelog.html (PL/EN/DE - same historyczne
    wpisy w tresci changeloga NIE zostaly ruszone, to poprawny zapis
    przeszlych wydan).
    
    tools/build-packages.ps1: $version podniesiony do 1.7.0 i uzyty do
    zbudowania realnych paczek dist/molique-1.7.0.zip (363 KB) oraz
    dist/molique-1.7.0-src.zip (500 KB) - obie wieksze niz stare etykiety,
    bo framework urosl miedzy wydaniami. Stare paczki 1.6.2 usuniete, zeby
    nie zostaly osierocone (download.html juz do nich nie linkuje).

--- tools/build-packages.ps1
+++ tools/build-packages.ps1
@@ -9,7 +9,7 @@
     powershell -File tools/build-packages.ps1
 #>
 
-$version = '1.6.2'
+$version = '1.7.0'
 
 # Katalog glowny repo (ten skrypt jest w tools/)
 $root = Split-Path -Parent $PSScriptRoot
